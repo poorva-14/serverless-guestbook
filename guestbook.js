157fc7d605321fb48a091eda5b74d6ac68ec5cc1
poorva-14 <63769006+poorva-14@users.noreply.github.com>
2022-06-05 13:20:46 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '/guestbook';
+const apiUrl = 'https://6618b37e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
